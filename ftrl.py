e850f152111da4d6b066338a9390810f353f68b4
jakubiakm <maciej190509@gmail.com>
2018-06-05 00:53:30 +0200

    Add empty FTRL class

---
+++ ftrl.py
@@ -0,0 +1,3 @@
+class FTRL:
+    def __init__(self):
+        print("FTRL execution start")
---
+++ main.py
@@ -0,0 +1,8 @@
+from ftrl import FTRL
+
+
+def main():
+    ftrl = FTRL()
+
+if __name__ == "__main__":
+    main()
