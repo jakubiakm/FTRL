b02fee200ab6326aa685b1e3087a0ec2438dbb7e
jakubiakm <maciej190509@gmail.com>
2018-06-08 15:46:13 +0200

    Change FTRL class form

--- ftrl.py
+++ ftrl.py
@@ -10,47 +10,27 @@ class FTRL:
         self.n = [0.] * 2 ** 28  
         self.z = [0.] * 2 ** 28
         self.w = {}
-        print("FTRL execution start")
 
-    def _indices(self, x):
-        # first yield index of the bias term
-        yield 0
-
-        # then yield the normal indices
-        for index in x:
-            yield index
-
-
-    def predict(self, x):
+    def predict(self, feature):
         w = {}
         wTx = 0
-        for i in self._indices(x):
-            sign = -1. if self.z[i] < 0 else 1.  # get sign of z[i]
-
-            # build w on the fly using z and n, hence the name - lazy weights
-            # we are doing this at prediction instead of update time is because
-            # this allows us for not storing the complete w
-            if sign * self.z[i] <= self.L1:
-                # w[i] vanishes due to L1 regularization
-                w[i] = 0.
+        for i in feature:
+            sign = 0 
+            if self.z[i] < 0:
+                sign = -1 
+            else: 
+                sign = 1
+            if abs(self.z[i]) <= self.L1:
+                self.w[i] = 0.
             else:
-                # apply prediction time L1, L2 regularization to z and get w
-                w[i] = (sign * self.L1 - self.z[i]) / ((self.beta + sqrt(self.n[i])) / self.alpha + self.L2)
+                self.w[i] = (-((self.beta + sqrt(self.n[i])) / self.alpha + self.L2) ** -1) * (self.z[i] - sign * self.L1)
+            wTx += self.w[i]
+        # ograniczenie funkcji exp, aby nie osiągała za dużych wartości (wtedy jej odwrotność jest bardzo bliska zeru)
+        return 1 / (1 + exp(-max(min(wTx, 35), -35)))
 
-            wTx += w[i]
-
-        # cache the current w for update stage
-        self.w = w
-
-        # bounded sigmoid function, this is the probability estimation
-        return 1. / (1. + exp(-max(min(wTx, 35.), -35.)))
-
-    def update(self, x, p, y):
-        # gradient under logloss
-        g = p - y
-
-        # update z and n
-        for i in self._indices(x):
-            sigma = (sqrt(self.n[i] + g * g) - sqrt(self.n[i])) / self.alpha
+    def update_model(self, feature, probability, clicked):
+        g = probability - clicked
+        for i in feature:
+            sigma = (sqrt(self.n[i] + g ** 2) - sqrt(self.n[i])) / self.alpha
             self.z[i] += g - sigma * self.w[i]
-            self.n[i] += g * g
+            self.n[i] += g ** 2
--- main.py
+++ main.py
@@ -10,50 +10,42 @@ def main(args):
     test(model)
 
 def load_data_enumarator(path):
-    for t, row in enumerate(DictReader(open(path), delimiter=',')):
-        try:
-            ID = row['ID']
-            del row['ID']
-        except:
-            pass 
-        y = 0.
-        target='target'
-        if target in row:
-            if row[target] == '1':
+    for _, row in enumerate(DictReader(open(path), delimiter=',')):
+        clicked = 0
+        if 'target' in row:
+            if row['target'] == '1':
                 #kliknięcię wystąpiło
-                y = 1.
-            del row[target]
-        # budowany jest wektor cech za pomocą hash trick
-        x = []
+                clicked = 1.
+        # budowa wektora cech za pomocą hash trick
+        feature = []
         for key in row:
-            value = row[key]
-            index = abs(hash(key + '_' + value)) % 2 ** 28   
-            x.append(index)
-        yield x, y
+            if key != 'target' and key != 'ID':
+                feature.append(abs(hash(key + '_' + row[key])) % 2 ** 28)
+        yield feature, clicked
 
 def train(ftrl_model):
     epoch = 10
     print('alpha:', ftrl_model.alpha, 'beta:', ftrl_model.beta, 'L1:', ftrl_model.L1, 'L2:', ftrl_model.L2)
     print('epoch, count, logloss')
-    for e in range(epoch):
+    for epoch_number in range(epoch):
         loss = 0
         count = 0
         train_data = load_data_enumarator('data/train.csv')
-        for x, y in train_data:  # data is a generator
-            p = ftrl_model.predict(x)
-            loss += logloss(p, y)
-            ftrl_model.update(x, p, y)
-            count+=1
-            if count%1000==0:
-                print(e + 1, count, loss/count)
+        for feature, clicked in train_data:  # data is a generator
+            probability = ftrl_model.predict(feature)
+            loss += logloss(probability, clicked)
+            ftrl_model.update_model(feature, probability, clicked)
+            count += 1
+            if count % 1000 == 0:
+                print(epoch_number + 1, count, loss/count)
 
 def test(ftrl_model):
     test_data = load_data_enumarator('data/test.csv')
     print ('write result')
     print('ID,target\n')
-    for x, y in test_data:
-        p = ftrl_model.predict(x)
-        print(p)
+    for feature, _ in test_data:
+        probability = ftrl_model.predict(feature)
+        print(probability)
 
 def logloss(p, y):
     return -log(p) if y == 1. else -log(1. - p)
@@ -64,6 +56,6 @@ if __name__ == "__main__":
     parser.add_argument('--beta', type=float, required=True, default=1)
     parser.add_argument('--L1', type=float, required=True, default=0)
     parser.add_argument('--L2', type=float, required=True, default=1)
-   
+
     args = parser.parse_args()
     main(args)
